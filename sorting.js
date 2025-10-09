4422a8015498ff3395d8a815838cc40860d280cd
Mokich <nokpsn2000@gmail.com>
2025-10-09 19:49:50 +0300

    soring

--- sorting.js
+++ sorting.js
@@ -9,3 +9,28 @@ const countries = [
   "Portugal", "Switzerland", "Austria", "Hungary", "Romania", "Bulgaria", "Serbia", "Croatia",
   "New Zealand", "United Arab Emirates"
 ];
+
+//Sort alphabetically
+const sortedAlphabetically = [...countries].sort((a ,b) => a.localeCompare(b));
+console.log("1. Sorted Alphabetically:", sortedAlphabetically);
+
+//reverse sort
+
+const sortedReverseAlphabetically = [...countries].sort((a ,b) => b.localeCompare(a));
+console.log("2. Sorted Alphabetically:", sortedReverseAlphabetically);
+
+//3. sort by length of country name (shortest to longest)
+const sortedByLenght = [...countries].sort((a ,b) => a.length - b.length);
+console.log("3. Sorted Size:", sortedByLenght);
+
+//4. sort by length of country name (longest to shortest)
+const sorteResversByLenght = [...countries].sort((a ,b) => b.length - a.length);
+console.log("3. Sorted Size:", sorteResversByLenght);
+
+//4. sort by of vowerls in country name 
+const countVowels = (str) => {
+    const vowels = 'aeiouAEIOU';
+    return str.split('').filter(letter => vowels.includes(letter)).length;
+};
+const sortedByVowelCount =[...countries].sort((a ,b) => countVowels(a)- countVowels(b));
+console.log("5.Sorted by amouts of vowels:", sortedByVowelCount)
