4e8cd4de1537ab7cea9492b88267bb46e8149e28
Mokich <nokpsn2000@gmail.com>
2025-10-09 19:46:41 +0300

    soring

---
+++ sorting.js
@@ -0,0 +1,11 @@
+// Array of world countries
+// Массив стран мира
+const countries = [
+  "Estonia", "Colombia", "China", "India", "Brazil", "Canada", "Australia", "Argentina", "Mexico", "France",
+  "Germany", "Italy", "Spain", "United Kingdom", "Japan", "South Korea", "Indonesia", "Russia",
+  "Egypt", "South Africa", "Saudi Arabia", "Turkey", "Iran", "Pakistan", "Bangladesh", "Vietnam",
+  "Thailand", "Philippines", "Malaysia", "USA", "Peru", "Chile", "Venezuela", "Ecuador", "Cuba",
+  "Belarus", "Ukraine", "Poland", "Czech Republic", "Sweden", "Norway", "Finland", "Denmark", "Greece",
+  "Portugal", "Switzerland", "Austria", "Hungary", "Romania", "Bulgaria", "Serbia", "Croatia",
+  "New Zealand", "United Arab Emirates"
+];
