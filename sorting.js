8dc183b0400ac67b7f0c68008dacf18c2da032af
Mokich <nokpsn2000@gmail.com>
2025-10-10 20:50:15 +0300

    sorting

--- sorting.js
+++ sorting.js
@@ -1,5 +1,5 @@
 // Array of world countries
-// Массив стран мира
+
 const countries = [
   "Estonia", "Colombia", "China", "India", "Brazil", "Canada", "Australia", "Argentina", "Mexico", "France",
   "Germany", "Italy", "Spain", "United Kingdom", "Japan", "South Korea", "Indonesia", "Russia",
