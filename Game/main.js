57d73fbf2174940ee2a5f7a7dbb6a6d5a809d36c
Mokich <nokpsn2000@gmail.com>
2025-10-16 17:20:51 +0300

    Games

---
+++ Game/main.js
@@ -0,0 +1,23 @@
+//Импортируем необходимые модули
+//import modules
+
+const readLineSync = require('readline-sync');
+
+//Контроллер игрового цикла
+//Game loop controller
+let keepPlaying = true;
+
+//Основной цикл меню. Будет продолжаться, пока игрок не захочет выйти
+//Main menu loop. Will continue until the player wants to exit
+while (keepPlaying) {
+    console.clear();
+    console.log("================================");
+    console.log("=      WELCOME TO THE GAME     =");
+    console.log("================================");
+    console.log("Which game mode do you want to play?");
+
+    const games = [
+        'Guess the Number'
+    ];    
+
+}
