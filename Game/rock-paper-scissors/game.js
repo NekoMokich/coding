0e36fca603a28ba061689b10faece647d466584f
Oleg Konnov <oleg.konnov@ivkhk.ee>
2025-10-21 18:27:18 +0300

    Games

---
+++ Game/rock-paper-scissors/game.js
@@ -0,0 +1,60 @@
+//import libary 
+const readlineSync = require('readline-sync');
+
+//Define player class
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.choices = null
+    }
+}  
+
+//define computer player class
+class Computer extends Player {
+    constructor() {
+        super('Computer');
+    }   
+    //methods for random choice moves for computer
+    makeRandomChoice() {
+        const randomIndex = Math.floor(Math.random() * options.length);
+        this.choices = options[randomIndex];
+    }
+}
+
+//define main game logic
+class RockPaperScissorsGame {
+    constructor() {
+        this.player = player;
+        this.computer = new Computer();
+        this.options = ['rock', 'paper', 'scissors'];
+        this.rules = {
+            rock: 'scissors',
+            paper: 'rock',
+            scissors: 'paper'
+        };
+    }
+    //method to start the game
+    start() {
+        console.log(`Welcome ${this.player.name} to Rock-Paper-Scissors Game!`);
+        this.playRound();
+    
+    }
+    //method to play one round
+    playRound() {
+        const choiceIndex = readlineSync.keyInSelect(this.options, 'Choose your move:');
+        
+        if (choiceIndex === -1) {
+            console.log('Game exited.');
+            return;
+        }
+        this.player.choices = this.options[choiceIndex];
+        this.computer.makeRandomChoice(this.choices);
+
+        console.log(`${this.player.name} chose: ${this.player.choices}`);
+        console.log(`Computer chose: ${this.computer.choices}`);
+
+        this.determineWinner();
+   
+    }
+
+}
